048ccffe2533ae1cd5ba0f6b59fb20d1b4076cee
mohamed osama <63753622+midotroy@users.noreply.github.com>
2024-09-16 19:07:47 +0300

    add new variables in config

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,6 @@ export default {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.DATABASE_URL as string,
   databaseDebug: process.env.DATABASE_DEBUG === 'true',
+  jwtSecretKey: process.env.JWT_SECRET as string,
+  jwtExpireDate:process.env.JWT_EXPIRES_IN as string
 };
