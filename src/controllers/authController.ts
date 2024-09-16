420dca7f9a6b851ba03d79dc3bda39ef30267fd9
mohamed osama <63753622+midotroy@users.noreply.github.com>
2024-09-16 19:04:04 +0300

    add Authentication controller and adding signup feature

---
+++ src/controllers/authController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { IAuthService } from '../interfaces/IAuthService';
+import { IUser } from '../interfaces/IUser';
+import { AuthService} from '../services/authService';
+import createToken from '../helper/createToken';
+
+export class AuthController { 
+    private authService: IAuthService;
+    constructor(authService:IAuthService= new AuthService()) {
+    this.authService = authService;
+  }
+    async signup(req: Request, res: Response){
+      const user = await this.authService.signup(req.body);
+      console.log(user);
+      
+      const token = createToken(user.id);
+      res.status(201).json({user:user,token:token});     
+    }
+}
