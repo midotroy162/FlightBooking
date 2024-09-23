e83ab52b8f3d4a118470f685be67b3e5c287de88
Mohamed Shehata <mohamed159716@yahoo.com>
2024-09-23 21:37:31 +0300

    Refactor signup method in AuthService to return ServiceResponse with token

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,13 +2,22 @@ import { IUser } from '../interfaces/IUser';
 import { IUserModel } from '../interfaces/IUserModel';
 import { IAuthService } from '../interfaces/IAuthService';
 import { UserModel } from '../models/userModel';
+import { ServiceResponse } from '../interfaces/IResponse';
+import { createServiceResponse } from './../helper/response';
+import createToken from '../helper/createToken';
 
 export class AuthService implements IAuthService {
-    private userModel: IUserModel;
-    constructor(userModel: IUserModel=new UserModel()) {
-        this.userModel = userModel;
+  private userModel: IUserModel;
+  constructor(userModel: IUserModel = new UserModel()) {
+    this.userModel = userModel;
+  }
+
+  async signup(user: IUser): Promise<ServiceResponse<IUser>> {
+    let data = await this.userModel.createUser(user);
+    if (!data) {
+      return createServiceResponse(false, 400, 'User creation failed', data);
     }
-    async signup(user: IUser): Promise<IUser> {
-        return this.userModel.createUser(user);
-    }
+    const token = createToken(data.id);
+    return createServiceResponse(true, 200, 'User created successfully', data, token);
+  }
 }
