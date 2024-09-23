080c48641abb4dee5e5f774cbd3713bcf3ef04ff
Mohamed Shehata <mohamed159716@yahoo.com>
2024-09-23 21:37:23 +0300

    Refactor authRoutes to include validation middleware for signup route

--- src/routes/api/authRoutes.ts
+++ src/routes/api/authRoutes.ts
@@ -1,10 +1,12 @@
 import { IRouter, Router } from 'express';
 import { AuthController } from '../../controllers/authController';
+import { validate } from '../../middlewares/validationMiddleware';
+import { UserValidator } from '../../validations/userValidator';
 
 const router: IRouter = Router();
 
-const  authController= new AuthController();
+const authController = new AuthController();
 
-router.post('/signup', authController.signup.bind(authController));
+router.post('/signup', validate(UserValidator.create), authController.signup.bind(authController));
 
 export default router;
