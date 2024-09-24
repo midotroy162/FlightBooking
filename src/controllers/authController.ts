dbf0ec146c7ac24addeef12176db997d734d3b1f
Mohamed Shehata <mohamed159716@yahoo.com>
2024-09-24 22:41:14 +0300

    Refactor authController to include validation middleware for user signup and login routes

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,27 +1,35 @@
 import { Request, Response } from 'express';
 import { IAuthService } from '../interfaces/IAuthService';
 import { IUser } from '../interfaces/IUser';
-import { AuthService} from '../services/authService';
+import { AuthService } from '../services/authService';
 import createToken from '../helper/createToken';
+import { createApiResponse } from '../helper/response';
 
-export class AuthController { 
-    private authService: IAuthService;
-    constructor(authService:IAuthService= new AuthService()) {
+export class AuthController {
+  private authService: IAuthService;
+  constructor(authService: IAuthService = new AuthService()) {
     this.authService = authService;
   }
-    async signup(req: Request, res: Response){
-      const user = await this.authService.signup(req.body);
-      console.log(user);
-      
-      const token = createToken(user.id);
-      res.status(201).json({user:user,token:token});     
+  async signup(req: Request, res: Response) {
+    const {
+      statusCode,
+      success,
+      message,
+      data,
+      accessToken: token,
+    } = await this.authService.signup(req.body);
+
+    return res.status(statusCode).json(createApiResponse(success, statusCode, message, data));
+    res.status(statusCode).json({ user: data, token: token });
   }
   async login(req: Request, res: Response) {
-    const user = await this.authService.login(req.body);
-    if (!user) {
-      res.json({message:""})
-    }
-    const token = createToken(user.id);
-      res.status(201).json({user:user,token:token});
+    const {
+      statusCode,
+      success,
+      message,
+      data,
+      accessToken: token,
+    } = await this.authService.login(req.body);
+    return res.status(statusCode).json(createApiResponse(success, statusCode, message, data));
   }
 }
