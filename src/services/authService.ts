e8412d532bba4b1165dff5c784c0b3b07fa395d8
mohamed osama <63753622+midotroy@users.noreply.github.com>
2024-09-24 16:16:28 +0300

    add login routes

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,7 @@ import { IUser } from '../interfaces/IUser';
 import { IUserModel } from '../interfaces/IUserModel';
 import { IAuthService } from '../interfaces/IAuthService';
 import { UserModel } from '../models/userModel';
+// import { compareSync } from 'bcrypt';
 
 export class AuthService implements IAuthService {
     private userModel: IUserModel;
@@ -10,5 +11,10 @@ export class AuthService implements IAuthService {
     }
     async signup(user: IUser): Promise<IUser> {
         return this.userModel.createUser(user);
+    }
+    async login(data:IUser):Promise<IUser>{
+        const user = await this.userModel.getUserByEmail(data.email);
+        if(!user|| !(data.password=== user.password)) throw new Error("Invalid email or password");
+        return user;
     }
 }
