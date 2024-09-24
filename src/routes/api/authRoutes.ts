e0d3ab9a0ad991b21ccc1e42a8a0329313f60ed3
mohamed osama <63753622+midotroy@users.noreply.github.com>
2024-09-24 16:16:18 +0300

    add login routes

--- src/routes/api/authRoutes.ts
+++ src/routes/api/authRoutes.ts
@@ -5,6 +5,7 @@ const router: IRouter = Router();
 
 const  authController= new AuthController();
 
-router.post('/signup', authController.signup.bind(authController));
+router.post('/signup', authController.signup.bind(authController))
+    .post('/login',authController.login.bind(authController));
 
 export default router;
