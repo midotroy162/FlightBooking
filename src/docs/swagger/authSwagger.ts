af2235bd011dc987f58d4f45511cd91f01b03f51
Mohamed Shehata <mohamed159716@yahoo.com>
2024-09-29 00:05:46 +0300

    Refactor authSwagger paths for plane company signup and login

--- src/docs/swagger/authSwagger.ts
+++ src/docs/swagger/authSwagger.ts
@@ -6,7 +6,7 @@ export const authSwagger = {
     },
   ],
   paths: {
-    '/auth/signup': {
+    '/auth/users/signup': {
       post: {
         summary: 'make new user signup',
         tags: ['Auth'],
@@ -19,31 +19,31 @@ export const authSwagger = {
                 properties: {
                   firstName: {
                     type: 'string',
-                    description: 'First name of the user'
+                    description: 'First name of the user',
                   },
                   lastName: {
                     type: 'string',
-                    description: 'Last name of the user'
+                    description: 'Last name of the user',
                   },
                   email: {
                     type: 'string',
                     format: 'email',
-                    description: 'User email address'
+                    description: 'User email address',
                   },
                   password: {
                     type: 'string',
                     format: 'password',
-                    description: 'User password'
+                    description: 'User password',
                   },
                   phone: {
                     type: 'string',
                     format: 'phone',
-                    description: 'User phone'
-                  }
-                }
-              }
-            }
-          }
+                    description: 'User phone',
+                  },
+                },
+              },
+            },
+          },
         },
         responses: {
           201: {
@@ -62,7 +62,7 @@ export const authSwagger = {
         },
       },
     },
-     '/auth/login': {
+    '/auth/users/login': {
       post: {
         summary: 'login user',
         tags: ['Auth'],
@@ -72,25 +72,142 @@ export const authSwagger = {
             'application/json': {
               schema: {
                 type: 'object',
-                properties: { 
+                properties: {
                   email: {
                     type: 'string',
                     format: 'email',
-                    description: 'User email address'
+                    description: 'User email address',
                   },
                   password: {
                     type: 'string',
                     format: 'password',
-                    description: 'User password'
+                    description: 'User password',
                   },
-                }
-              }
-            }
-          }
+                },
+              },
+            },
+          },
         },
         responses: {
           201: {
             description: 'Login user',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    $ref: '#/components/schemas/login',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    '/auth/planes/companies/signup': {
+      post: {
+        summary: 'make new plane company signup',
+        tags: ['Auth'],
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  name: {
+                    type: 'string',
+                    description: 'Name of the plane company',
+                    example: 'Airline Company',
+                  },
+                  code: {
+                    type: 'string',
+                    description: 'Code of the plane company',
+                    example: 'AC',
+                  },
+                  email: {
+                    type: 'string',
+                    format: 'email',
+                    description: 'Plane company email address',
+                    example: 'ac@mail.com',
+                  },
+                  password: {
+                    type: 'string',
+                    format: 'password',
+                    description: 'Plane company password',
+                    example: 'Password@123',
+                  },
+                  confirmPassword: {
+                    type: 'string',
+                    format: 'password',
+                    description: 'Confirm plane company password',
+                    example: 'Password@123',
+                  },
+                  phone: {
+                    type: 'string',
+                    format: 'phone',
+                    description: 'Plane company phone',
+                    example: '+201022222221',
+                  },
+                  address: {
+                    type: 'string',
+                    description: 'Plane company address',
+                    example: 'Cairo, Egypt',
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: 'the new plane company created',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    $ref: '#/components/schemas/PlaneCompany',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    '/auth/planes/companies/login': {
+      post: {
+        summary: 'login plane company',
+        tags: ['Auth'],
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  email: {
+                    type: 'string',
+                    format: 'email',
+                    description: 'Plane company email address',
+                    example: 'ac@mail.com',
+                  },
+                  password: {
+                    type: 'string',
+                    format: 'password',
+                    description: 'Plane company password',
+                    example: 'Password@123',
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: 'Login plane company',
             content: {
               'application/json': {
                 schema: {
@@ -150,9 +267,8 @@ export const authSwagger = {
       },
       login: {
         type: 'object',
-        required: [ 'email', 'password'],
+        required: ['email', 'password'],
         properties: {
-          
           email: {
             type: 'string',
             description: 'The email of the user',
@@ -160,6 +276,50 @@ export const authSwagger = {
           password: {
             type: 'string',
             description: 'The password of the user',
+          },
+        },
+      },
+      PlaneCompany: {
+        type: 'object',
+        required: ['id', 'email', 'password'],
+        properties: {
+          id: {
+            type: 'integer',
+            description: 'The auto-generated id of the plane company',
+          },
+          email: {
+            type: 'string',
+            description: 'The email of the plane company',
+          },
+          password: {
+            type: 'string',
+            description: 'The password of the plane company',
+          },
+          name: {
+            type: 'string',
+            description: 'The name of the plane company',
+          },
+          code: {
+            type: 'string',
+            description: 'The code of the plane company',
+          },
+          phoneNumber: {
+            type: 'string',
+            description: 'The phone number of the plane company',
+          },
+          address: {
+            type: 'string',
+            description: 'The address of the plane company',
+          },
+          createdAt: {
+            type: 'string',
+            format: 'date-time',
+            description: 'The date the plane company was created',
+          },
+          updatedAt: {
+            type: 'string',
+            format: 'date-time',
+            description: 'The date the plane company was last updated',
           },
         },
       },
