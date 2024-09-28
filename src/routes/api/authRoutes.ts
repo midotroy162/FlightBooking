5f600f2fadd85950455e840faa73854ed6ab77dd
Mohamed Shehata <mohamed159716@yahoo.com>
2024-09-29 00:04:58 +0300

    add basic plane company auth

--- src/routes/api/authRoutes.ts
+++ src/routes/api/authRoutes.ts
@@ -1,14 +1,29 @@
 import { IRouter, Router } from 'express';
-import { AuthController } from '../../controllers/authController';
+import { AuthController, PlaneCompanyAuthController } from '../../controllers/authController';
 import { validate } from '../../middlewares/validationMiddleware';
 import { UserValidator } from '../../validations/userValidator';
+import { AuthValidator } from '../../validations/authValidator';
+import { PlaneCompanyValidator } from '../../validations/planeCompanyValidator';
 
 const router: IRouter = Router();
 
 const authController = new AuthController();
+const planeCompanyAuthController = new PlaneCompanyAuthController();
 
 router
-  .post('/signup', validate(UserValidator.create), authController.signup.bind(authController))
-  .post('/login', authController.login.bind(authController));
+  .post('/users/signup', validate(UserValidator.create), authController.signup.bind(authController))
+  .post('/users/login', validate(AuthValidator.Login), authController.login.bind(authController));
+
+router
+  .post(
+    '/planes/companies/signup',
+    validate(PlaneCompanyValidator.create),
+    planeCompanyAuthController.signup.bind(planeCompanyAuthController),
+  )
+  .post(
+    '/planes/companies/login',
+    validate(AuthValidator.Login),
+    planeCompanyAuthController.login.bind(planeCompanyAuthController),
+  );
 
 export default router;
