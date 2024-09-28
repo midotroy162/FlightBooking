685b4b428b2f11a966f6dbcb04d852fbbb9ba778
Mohamed Shehata <mohamed159716@yahoo.com>
2024-09-29 00:05:05 +0300

    Add IPlaneCompanyService interface for plane company authentication

--- src/interfaces/IAuthService.ts
+++ src/interfaces/IAuthService.ts
@@ -1,6 +1,12 @@
+import { IPlaneCompany } from './IPlaneCompany';
 import { ServiceResponse } from './IResponse';
 import { IUser } from './IUser';
 export interface IAuthService {
   signup(user: IUser): Promise<ServiceResponse<IUser>>;
   login(user: IUser): Promise<ServiceResponse<IUser>>;
+}
+
+export interface IPlaneCompanyService {
+  signup(planeCompany: IPlaneCompany): Promise<ServiceResponse<IPlaneCompany>>;
+  login(planeCompany: IPlaneCompany): Promise<ServiceResponse<IPlaneCompany>>;
 }
