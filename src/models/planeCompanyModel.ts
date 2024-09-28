782a0e4a6e8349addd2707a0799fb44ef1bb6c0b
Mohamed Shehata <mohamed159716@yahoo.com>
2024-09-29 00:05:13 +0300

    Add PlaneCompanyModel for managing plane company data

---
+++ src/models/planeCompanyModel.ts
@@ -0,0 +1,18 @@
+import { eq } from 'drizzle-orm';
+import { db } from '../database';
+import { IPlaneCompany } from '../interfaces/IPlaneCompany';
+import { IPlaneCompanyModel } from '../interfaces/IPlaneCompanyModel';
+import { planeCompanies } from '../schema';
+
+export class PlaneCompanyModel implements IPlaneCompanyModel {
+  async createPlaneCompany(planeCompany: IPlaneCompany): Promise<IPlaneCompany> {
+    return (
+      await db.insert(planeCompanies).values(planeCompany).returning().execute()
+    )[0] as IPlaneCompany;
+  }
+  async getPlaneCompanyByEmail(email: string): Promise<IPlaneCompany | null> {
+    return (await db.query.planeCompanies.findFirst({
+      where: eq(planeCompanies.email, email),
+    })) as IPlaneCompany | null;
+  }
+}
