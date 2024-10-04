41095a92b59160396c849e8a70014b984f0378d3
Mohamed Shehata <mohamed159716@yahoo.com>
2024-10-04 12:03:36 +0300

    Refactor IPlaneCompany interface and add ICommonIPlaneCompany interface

--- src/interfaces/IPlaneCompany.ts
+++ src/interfaces/IPlaneCompany.ts
@@ -1,13 +1,18 @@
-export interface IPlaneCompany {
+interface ICommonIPlaneCompany {
   id: number;
   name: string;
   code: string;
   email: string;
   phoneNumber: string;
-  password: string;
   address: string;
   isEmailVerified: boolean;
   isPhoneVerified: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IPlaneCompany extends ICommonIPlaneCompany {
+  password: string;
   passwordChangedAt: Date;
   emailVerificationToken: string;
   emailVerificationTokenExpires: Date;
@@ -15,11 +20,11 @@ export interface IPlaneCompany {
   phoneVerificationCodeExpires: Date;
   passwordResetToken: string;
   passwordResetTokenExpires: Date;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
 export interface IPlaneCompanyModel {
   createPlaneCompany(planeCompany: IPlaneCompany): Promise<IPlaneCompany>;
   getPlaneCompanyByEmail(email: string): Promise<IPlaneCompany | null>;
 }
+
+export interface IPlaneCompanyResponse extends ICommonIPlaneCompany {}
